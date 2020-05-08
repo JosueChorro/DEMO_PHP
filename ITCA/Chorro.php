2242954fe139e7d5e56c1eb79b25e24549f02e84
Josue Chorro <josue14149@gmail.com>
2020-05-08 13:08:07 -0600

    Creacion de la carpeta ITCA y mis ficheros

---
+++ ITCA/Chorro.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<p>Tambien he avanzando en la creacion de proyectos</p>";
+	echo "<p>sociales que ayuden a mejorar la situacion de </p>";
+	echo "<p>Zacatecoluca post pandemia con la ayuda de los scout</p>";
+
+?>
---
+++ ITCA/Josue.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<p>Hola que tal?, todo bien?, todo correcto?</p><br>";
+	echo "<p>He estado aprovechado este tiempo libre para </p>";
+	echo "<p>practicar lenguajes de programacion como angular,</p>";
+	echo "<p>python y c++</p>";
+?>
